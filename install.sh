0a48b60d9ae136ab1b3a47da618838816d0ea3ee
Masaki Komagata <komagata@gmail.com>
2026-05-21 02:41:51 +0900

    Add PR report banner

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-VERSION="${FLAKEWATCH_VERSION:-v0.6.1}"
+VERSION="${FLAKEWATCH_VERSION:-v0.6.2}"
 INSTALL_DIR="${FLAKEWATCH_INSTALL_DIR:-/usr/local/bin}"
 TMP_DIR="${TMPDIR:-/tmp}"
 
