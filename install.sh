a07f7793b4c0a9301706bd9bf123a19575f0a87e
Masaki Komagata <komagata@gmail.com>
2026-05-21 14:02:24 +0900

    Replace report banner with compact button

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-VERSION="${FLAKEWATCH_VERSION:-v0.6.3}"
+VERSION="${FLAKEWATCH_VERSION:-v0.6.4}"
 INSTALL_DIR="${FLAKEWATCH_INSTALL_DIR:-/usr/local/bin}"
 TMP_DIR="${TMPDIR:-/tmp}"
 
