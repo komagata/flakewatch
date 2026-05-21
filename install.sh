f7680a6473e7b90f0b4aa67e735957755acfc63b
Masaki Komagata <komagata@gmail.com>
2026-05-21 13:47:33 +0900

    Shrink PR report banner

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-VERSION="${FLAKEWATCH_VERSION:-v0.6.2}"
+VERSION="${FLAKEWATCH_VERSION:-v0.6.3}"
 INSTALL_DIR="${FLAKEWATCH_INSTALL_DIR:-/usr/local/bin}"
 TMP_DIR="${TMPDIR:-/tmp}"
 
