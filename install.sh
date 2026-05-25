52d9e02aa0103290dd8561af2ca2ea5d38e64bad
Masaki Komagata <komagata@gmail.com>
2026-05-25 18:08:33 +0900

    Prepare v0.6.10 release

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-VERSION="${FLAKEWATCH_VERSION:-v0.6.9}"
+VERSION="${FLAKEWATCH_VERSION:-v0.6.10}"
 INSTALL_DIR="${FLAKEWATCH_INSTALL_DIR:-/usr/local/bin}"
 TMP_DIR="${TMPDIR:-/tmp}"
 
