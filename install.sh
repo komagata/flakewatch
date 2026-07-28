6465483d32e8d983e606983f552e619ccd0120f5
Masaki Komagata <komagata@gmail.com>
2026-07-29 01:49:24 +0900

    Reduce report aggregation memory

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-VERSION="${FLAKEWATCH_VERSION:-v0.6.35}"
+VERSION="${FLAKEWATCH_VERSION:-v0.6.36}"
 INSTALL_DIR="${FLAKEWATCH_INSTALL_DIR:-/usr/local/bin}"
 TMP_DIR="${TMPDIR:-/tmp}"
 
